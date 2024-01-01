00504c1cb8815e26b0d297f00656a35d6d72a765
Persephone <its.persephone.xo@gmail.com>
2024-01-01 15:12:52 -0500

    basic starter files created

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//start using example for the date-fns library
+/*
+import { formatDistance, subDays } from "date-fns";
+
+formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true });
+//=> "3 days ago"
+*/
