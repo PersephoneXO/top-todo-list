192292f3ff0cc53a3eefea731fbf505557951809
Persephone <its.persephone.xo@gmail.com>
2024-01-03 16:26:56 -0500

    started the base html file and figured out the design

--- src/todos.js
+++ src/todos.js
@@ -1,4 +1,16 @@
-//module for creating todo objects
-const createTodo=(title,description,dueDate,priority,checklist)=>{
-    return {title,description,dueDate,priority,checklist};
-}
+//module for creating and manipulating todo objects
+
+//difference in days from date-fns
+const { differenceInDays,format } = require("date-fns");
+
+//create a todo object
+const createTodo=(title,description,deadline,priority,projectName)=>{
+    //let todayDate=format(new Date(),"cccc LLLL d, yyyy");
+    let checklist=false;
+    return {title,description,deadline,priority,checklist,projectName};
+};
+
+//create a project object
+const createProject=(title,todos=[])=>{
+    return {title,todos};
+};
